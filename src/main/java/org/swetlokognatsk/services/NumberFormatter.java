bb7a46acb73cc2e2ac05dc815141c2db6b3983d4
Slowpool <nevznachay02@gmail.com>
2026-05-28 16:53:19 +0300

    last steps. base 2 is abandoned

--- src/main/java/org/swetlokognatsk/services/NumberFormatter.java
+++ src/main/java/org/swetlokognatsk/services/NumberFormatter.java
@@ -15,15 +15,19 @@ public class NumberFormatter {
      */
     public static String format(int mantissa, int basis, int exponent) {
         var formattedNumber = switch (basis) {
+        case 2 -> {
+            // yield formatNumberWithBase2(mantissa, exponent);
+            throw new RuntimeException("not implemented");
+        }
         case 10 -> {
-            yield formatNumberWithBase10(mantissa, basis, exponent);
+            yield formatNumberWithBase10(mantissa, exponent);
         }
         default -> throw new RuntimeException("this base is not implemented");
         };
         return formattedNumber;
     }
 
-    protected static String formatNumberWithBase10(int mantissa, int basis, int exponent) {
+    protected static String formatNumberWithBase10(final int mantissa, final int exponent) {
         var unsignedNumber = String.valueOf(Math.abs(mantissa));
         var numberBuilder = new StringBuilder(unsignedNumber);
         var integerExponent = Integer.valueOf(exponent);
@@ -69,11 +73,52 @@ public class NumberFormatter {
             var zeros = buildZeros(numberOfZeros);
             number.append(zeros);
             number.insert(number.length() - 1, POINT);
-        }
-        else {
+        } else {
             number.insert(BASE_POSITION_OF_POINT + exponent, POINT);
         }
 
+    }
+
+    protected static String formatNumberWithBase2(final int mantissa, final int exponent) {
+        var absMantissa = Math.abs(mantissa);
+        var integerExponent = Integer.valueOf(exponent);
+
+        int leftPart;
+        int rightPart;
+        switch (integerExponent.compareTo(0)) {
+        case -1:
+            // it does't work so, because number can be any big, so it requires casting to virtual binary. implementing it via List<Bit> is a good idea, but i already grasped how it works and it would be redundant to implement it
+            leftPart = absMantissa << (32 - exponent);
+            rightPart = absMantissa >> (exponent);
+            break;
+        case 0:
+            // first bit is sign, so it's skipped
+            var theMostLeftBit = 0b01000000000000000000000000000000;
+            // defense from infinite loop
+            int bitsToCheck = 31;
+            int i = 0;
+            while ((theMostLeftBit & absMantissa) != 0b1 && i < bitsToCheck) {
+                theMostLeftBit >>= 1;
+                i++;
+            }
+            leftPart = theMostLeftBit;
+            rightPart = (~theMostLeftBit) & absMantissa;
+            break;
+        case 1:
+            leftPart = 1;
+            rightPart = 1;
+            break;
+            default:
+                throw new RuntimeException("unreachable code");
+        }
+
+        var numberBuilder = new StringBuilder();
+
+        if (mantissa < 0) {
+            numberBuilder.insert(0, MINUS);
+        }
+
+        return leftPart + "." + rightPart;
     }
 
     protected static String buildZeros(int numberOfZeros) {
